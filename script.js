21b59e2172ad7eb9d75701801e0aafdae4eb7fdc
kirkkenney <38423409+kirkkenney@users.noreply.github.com>
2018-06-29 11:24:11 +0100

    Add "Change To Linear" function

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var css = document.querySelector("h3");
+var rgbOut = document.querySelector("h4");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("background");
 var random = document.querySelector(".random");
 var radial = document.querySelector(".radial");
+var linear = document.querySelector(".linear");
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, "
 	+ color1.value + ", " + color2.value + ")";
-	css.innerHTML = "Colour 1 = " + color1.value + " : " + "Colour 2 = " + color2.value;
+	css.textContent = "Colour 1 = " + color1.value + " : " + "Colour 2 = " + color2.value;
 }
 function randomColours() {
 	color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -17,11 +19,12 @@ function randomColours() {
 }
 function radialGradient() {
 	body.style.background = "radial-gradient(" + color1.value + ", " + color2.value + ")";
-	css.innerHTML = "Colour 1 = " + color1.value + " : " + "Colour 2 = " + color2.value;
+	css.textContent = "Colour 1 = " + color1.value+ " : " + "Colour 2 = " + color2.value;
 }
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 random.addEventListener("click", randomColours);
 radial.addEventListener("click", radialGradient);
+linear.addEventListener("click", setGradient);
 
 window.onload = randomColours();
